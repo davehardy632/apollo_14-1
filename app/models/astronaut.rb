87801d91e6edceffd3e4050f70d17644771cd2d0
David Hardy <davehardy632@gmail.com>
2019-05-10 10:01:08 -0600

    Friday challenge submission

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -1,3 +1,19 @@
 class Astronaut < ApplicationRecord
+  has_many :astronaut_missions
+  has_many :missions, through: :astronaut_missions
   validates_presence_of :name, :age, :job
+
+
+  def self.average_age
+    average(:age)
+  end
+
+  def order_missions
+    missions.order(:title)
+  end
+
+  def total_time_in_space
+    missions.sum(:time_in_space)
+  end
+
 end
